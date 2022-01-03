df4a1469f72455e833f601e5024e7f3f4485743c
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2022-01-03 17:09:59 -0500

    correcion error deploy

--- src/helper/apiClient.js
+++ src/helper/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = () => {
     const axiosInstance = axios.create({
-        baseURL: "https://clinica-reactiva.herokuapp.com/citas", 
+        baseURL: "https://clinica-reactiva.herokuapp.com", 
         responseType: "json"
     })
 
